7f7ef0d5a9a3cb3faff6f118777b34775ac3a5b0
IanMancuso <Ian.mancuso@gmail.com>
2022-02-02 20:33:01 -0500

    Updated Basket Script

--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -14,4 +14,11 @@ using UnityEngine;
         pos.x = mousePos3D.x;
         this.transform.position = pos;
     }
+    void OnCollisionEnter( Collision coll ) {                         // a
+        // Find out what hit this basket
+        GameObject collidedWith = coll.gameObject;                    // b
+        if ( collidedWith.tag == "Apple" ) {                          // c
+            Destroy( collidedWith );
+        }
+    }
  }
