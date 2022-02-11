5b0767afac5251b791c5d2210f904455fa9606ef
IanMancuso <Ian.mancuso@gmail.com>
2022-02-11 13:21:28 -0500

    Slingshot can create projectiles

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -2,11 +2,19 @@ using UnityEngine;
 using System.Collections;
 
 public class Slingshot : MonoBehaviour {
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+
+    [Header("Set Dynamically")]
     public GameObject launchPoint;
+    public Vector3             launchPos;                                 
+    public GameObject          projectile;                                
+    public bool                aimingMode;
     void Awake() {
         Transform launchPointTrans = transform.Find("LaunchPoint");              
         launchPoint = launchPointTrans.gameObject;
-        launchPoint.SetActive( false );                                          
+        launchPoint.SetActive( false );
+        launchPos = launchPointTrans.position;                                
     }
     void OnMouseEnter() {
         //print("Slingshot:OnMouseEnter()");
@@ -15,5 +23,15 @@ public class Slingshot : MonoBehaviour {
     void OnMouseExit() {
         //print("Slingshot:OnMouseExit()");
         launchPoint.SetActive( false );                                          
+    }
+    void OnMouseDown() {                                                
+        // The player has pressed the mouse button while over Slingshot
+        aimingMode = true;
+        // Instantiate a Projectile
+        projectile = Instantiate( prefabProjectile ) as GameObject;
+        // Start it at the launchPoint
+        projectile.transform.position = launchPos;
+        // Set it to isKinematic for now
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
     }
  }
